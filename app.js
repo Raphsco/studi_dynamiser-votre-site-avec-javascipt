d477ecebd722d066df822e6c16a29ebdf0a761c9
raphael <scottodipertaraphael@gmail.com>
2021-05-29 20:53:52 +0200

    fin des premiere modification js
    utilisation  math floor et math.random + if else

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         var dice = Math.floor(Math.random() * 6) + 1;
 
         var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+
+       
+        if (dice !== 1) {
+            
+            scoresTours += dice;
+            document.querySelector('#current-' + JoueurActif).textContent = scoresTours;
+        } else {
+           
+            nextPlayer();
+        }
+    }    
+});
